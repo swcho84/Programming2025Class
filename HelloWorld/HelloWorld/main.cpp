339b528c2d1381d0006746b22bbdee42a2773e15
Sungwook Cho <swcho84@gmail.com>
2025-09-15 15:56:41 +0900

    ex1_upload

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -1,7 +1,18 @@
+#pragma warning(disable:4996)
+
 #include <stdio.h>
+#define _CRT_SECUER_NO_WARNINGS 
 
 int main()
 {
-	printf("Hello, World!\n");
+	// practice#1: print "Hello, World!" to the console
+	// printf("Hello, World!\n");
+
+	// practice#2: input an integer from the console, and then print it to the console
+	int nA = 0;
+	printf("input an integer: ");
+	scanf("%d", &nA);
+	printf("input_result: %d\n", nA);
+
 	return 0;
 }
---
+++ ex1_printf_only/ex1_printf_only/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+	// 1st example practice: printf only
+	printf("");
+	printf("");
+	printf("");
+	printf("");
+	printf("");
+	return 0;
+}
